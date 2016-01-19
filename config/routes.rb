0695bf49ef8d496fd65f1d7d4b5532d247c2f2b1
Kyle Bedell <kgbedell@gmail.com>
2016-01-18 22:58:09 -0800

    Update routes. Add Devise i18n. Add Devise initializer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,19 @@
 Forgecms::Engine.routes.draw do
+
+  devise_for :user, class_name: "Forgecms::User", module: :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  scope :admin do
+    get '/', to: 'dashboard#index', as: :dashboard
+    resources :posts
+  end
+
+  devise_scope :user do
+    get "/signup" => "/users/registrations#new"
+    get "/login" => "/users/sessions#new"
+    get "/logout" => "/users/sessions#destroy"
+  end
+
+
 end
