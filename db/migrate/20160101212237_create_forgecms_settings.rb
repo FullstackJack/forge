f7eea4e005151bf9b80ebeaca52dfbacb84ec02e
Kyle Bedell <kgbedell@gmail.com>
2016-01-21 21:56:11 -0800

    Change field name.

--- db/migrate/20160101212237_create_forgecms_settings.rb
+++ db/migrate/20160101212237_create_forgecms_settings.rb
@@ -1,7 +1,7 @@
 class CreateForgecmsSettings < ActiveRecord::Migration
   def change
     create_table :forgecms_settings do |t|
-      t.string :label
+      t.string :name
       t.string :value
       t.timestamps null: false
     end
