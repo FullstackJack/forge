3f6ef691cdb1615d7fb6203143b393af78b6d56c
Kyle Bedell <kgbedell@gmail.com>
2016-01-26 18:18:30 -0800

    Add model validation.

--- app/models/forgecms/tag.rb
+++ app/models/forgecms/tag.rb
@@ -1,4 +1,8 @@
 module Forgecms
   class Tag < ActiveRecord::Base
+    has_many :forgecms_tag_links, dependent: :destroy, class_name: 'Forgecms::TagLink'
+    has_many :forgecms_posts, through: :tag_links, class_name: 'Forgecms::Post'
+
+    validates :name, presence: true
   end
 end
