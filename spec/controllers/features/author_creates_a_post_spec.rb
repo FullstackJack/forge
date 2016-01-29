cd4258e40e87676321464f5e25a096d4d0c10544
Kyle Bedell <kgbedell@gmail.com>
2016-01-28 16:07:14 -0800

    Update spec test.

--- spec/controllers/features/author_creates_a_post_spec.rb
+++ spec/controllers/features/author_creates_a_post_spec.rb
@@ -4,7 +4,10 @@ feature 'Author creates a post' do
   scenario 'they see the post on the edit page' do
     visit forgecms.new_post_path
 
-    fill_in
+    fill_in 'title', with: 'My First Post'
+    fill_in 'body', with: '<p>Hello, world.</p>'
+    click_button '.submit'
+
     expect(page).to have_css '.post'
   end
 end
