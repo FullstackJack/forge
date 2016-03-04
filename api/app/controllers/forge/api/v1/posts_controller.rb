a17f998f7dde2211cbe919c56b7f41e64dbcb2be
Kyle Bedell <kgbedell@gmail.com>
2016-03-04 00:04:37 -0800

    Fix indent issue.

--- api/app/controllers/forge/api/v1/posts_controller.rb
+++ api/app/controllers/forge/api/v1/posts_controller.rb
@@ -5,55 +5,55 @@ module Forge
 
         def index
           @posts = Post.all
-					authorize @posts
-					render json: @posts
-				end
+          authorize @posts
+          render json: @posts
+        end
 
-				def create
-					@post = current_forge_user.posts.build(post_params)
-					authorize @post
-					if @post.save
-						render json: @post, status: :created
-					else
-					  render json: {
-							errors: [
-								{
+        def create
+          @post = current_forge_user.posts.build(post_params)
+          authorize @post
+          if @post.save
+            render json: @post, status: :created
+          else
+            render json: {
+              errors: [
+                {
                   status: 400,
                   title: "Not Saved",
-									detail: "The post was not saved."
-								}
-							]
-					  }, status: :bad_request
-					end
-				end
+                  detail: "The post was not saved."
+                }
+              ]
+            }, status: :bad_request
+          end
+        end
 
-				def update
-					post = Post.find(params[:id])
-					authorize post
-					if post.update(post_params)
-					 render json: post
-					end
-				end
+        def update
+          post = Post.find(params[:id])
+          authorize post
+          if post.update(post_params)
+           render json: post
+          end
+        end
 
-				def show
-					post = Post.find(params[:id])
-					authorize post
-					render json: post
-				end
+        def show
+          post = Post.find(params[:id])
+          authorize post
+          render json: post
+        end
 
-				def destroy
-					post = Post.find(params[:id])
-					authorize post
-					if post.destroy
-						head :no_content
-					end
-				end
+        def destroy
+          post = Post.find(params[:id])
+          authorize post
+          if post.destroy
+            head :no_content
+          end
+        end
 
-				private
+        private
 
-				def post_params
-					params.require(:post).permit(:title, :body, :published_at, :slug)
-				end
+        def post_params
+          params.require(:post).permit(:title, :body, :published_at, :slug)
+        end
       end
     end
   end
